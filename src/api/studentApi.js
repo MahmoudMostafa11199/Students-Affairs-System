4865d97603f393d16601e9255310655f0dff8ac5
Mahmoud Mostafa <m_devolepment@hotmail.com>
2026-02-13 23:12:34 +0200

    added modal form and add new student

--- src/api/studentApi.js
+++ src/api/studentApi.js
@@ -1,6 +1,8 @@
 import { API_URL } from '../utils/constant.js';
 import { getCourses } from './courseApi.js';
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
+// Fetch student
 export const getStudents = async () => {
   try {
     const res = await fetch(`${API_URL}/students`);
@@ -23,6 +25,31 @@ export const getStudents = async () => {
     });
 
     return students;
+
+    //
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+// Create student
+export const createStudent = async (data) => {
+  try {
+    const stdId = `std-${uuidv4().substring(0, 8)}`;
+
+    const newStudent = {
+      id: stdId,
+      ...data,
+      courses: [data.courses],
+    };
+
+    const res = await fetch(`${API_URL}/students`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newStudent),
+    });
+
+    
 
     //
   } catch (err) {
--- src/models/AddStudent.js
+++ src/models/AddStudent.js
@@ -1,7 +1,13 @@
+import { getCourses } from '../api/courseApi.js';
+import { createStudent, getStudents } from '../api/studentApi.js';
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const showModal = document.querySelector('.btn--add');
 const btnCloseModal = document.querySelector('.close-modal');
+const formCancel = document.querySelector('.form-cancel');
+const selectCourses = document.querySelector('.std-courses');
+const formAdd = document.querySelector('.modal-form');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -14,8 +20,8 @@ const closeModal = function () {
 };
 
 showModal.addEventListener('click', openModal);
-
 btnCloseModal.addEventListener('click', closeModal);
+formCancel.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
@@ -23,3 +29,77 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//
+let students;
+let courses;
+
+//
+formAdd.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formInputs = e.target.querySelectorAll('.form-input');
+
+  for (const input of formInputs) {
+    if (!input.value.trim()) {
+      document.querySelector('.error-' + input.name).textContent =
+        input.name + ' required';
+
+      //
+    } else {
+      document.querySelector('.error-' + input.name).textContent = '';
+    }
+  }
+
+  const isEmptyInput = [...formInputs].some((input) => !input.value.trim());
+
+  if (isEmptyInput) return;
+
+  if (
+    isNaN(Number(formInputs[1].value)) ||
+    formInputs[1].value < 7 ||
+    formInputs[1].value > 70
+  ) {
+    document.querySelector('.error-age').textContent = 'Age is invalid';
+    return;
+  }
+
+  if (isNaN(Number(formInputs[4].value))) {
+    document.querySelector('.error-level').textContent = 'Level is invalid';
+    return;
+  }
+
+  const existingEmail = students.some((std) => {
+    return std.email == formInputs[2].value.trim();
+  });
+
+  if (existingEmail) {
+    document.querySelector('.error-email').textContent = 'Email already exists';
+    return;
+  }
+
+  const newStudent = {
+    name: formInputs[0].value,
+    age: +formInputs[1].value,
+    email: formInputs[2].value,
+    courses: formInputs[3].value,
+    level: +formInputs[4].value,
+  };
+
+  createStudent(newStudent);
+  closeModal();
+});
+
+// load courses
+const load = async () => {
+  courses = await getCourses();
+  students = await getStudents();
+
+  courses.map((crs) => {
+    const opt = `<option value="${crs.id}">${crs.title}</option>`;
+
+    selectCourses.insertAdjacentHTML('beforeend', opt);
+  });
+};
+
+load();
