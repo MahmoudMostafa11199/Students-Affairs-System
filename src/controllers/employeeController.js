9be5edc44443e5cb088b4433be3fb143b1ef69cf
Ahmed Elhendawy <ahmed.elhendawy524@gmail.com>
2026-02-15 03:19:24 +0200

    Get Employees Data And displat it

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -1,18 +1,19 @@
-// import * as api from "../api/employeeApi.js";
+import * as api from "../api/employeeApi.js";
 import * as Employee from "../models/Employee.js";
 
-// const controlCourses = async function () {
-//   try {
-// const data = await api.getCourses();
-document.querySelector(".table__body").innerHTML = "";
-Employee.render();
-Employee.sortCourse();
-Employee.search();
+const controlCourses = async function () {
+  try {
+    const data = await api.getEmployees();
 
-//
-//   } catch (err) {
-// console.error("Controller Error 💥:", err);
-//   }
-// };
+    document.querySelector(".table__body").innerHTML = "";
+    Employee.render(data);
+    Employee.sortCourse(data);
+    Employee.search(data);
 
-// controlCourses();
+    //
+  } catch (err) {
+    console.error("Controller Error 💥:", err);
+  }
+};
+
+controlCourses();
